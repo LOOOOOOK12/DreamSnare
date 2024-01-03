cc4c66f2ede130ba26bce35029fac72b785e3cbf
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-03 16:58:26 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Navbar from "../Components/Navbar"
 
 function Home() {
     return (
         <div className='bg-background'>
-
+            <Navbar/>
         </div>
     )
 }
