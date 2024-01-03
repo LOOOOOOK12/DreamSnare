ad975edd0b55dd1d5f872e074c5c3cd9d7323273
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-03 16:58:17 +0800

    update

--- Likod/server.js
+++ Likod/server.js
@@ -5,6 +5,29 @@ const cors = require("cors")
 const app = express()
 app.use(cors());
 
-app.listen(8001, ()=>{
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:"root",
+    password:'',
+    database:'website'
+})
+
+app.post('/signup',(req,res) => {
+    const sql = "INSERT INTO user (`username`,`email`,`password`) Values (?) "
+    const values = [
+        req.body.username,
+        req.body.email,
+        req.body.password,
+    ]
+    db.query(sql, [values], (err,data) =>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+
+})
+
+app.listen(8001, ()=> {
     console.log("Server is Open")
 })
