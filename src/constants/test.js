cd6691533b92ddbbb3383a4643f292b15e9f259e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-04 16:03:51 +0800

    update

---
+++ src/constants/test.js
@@ -0,0 +1,27 @@
+export const TestData = [
+    {
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+    {
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+    {
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+    {
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+    {
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+]
