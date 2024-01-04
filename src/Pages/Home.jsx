ae92508d25dfc1fa8238d7f48b0a10ad1498e450
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-04 16:03:55 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,20 @@
 import Navbar from "../Components/Navbar"
+import DreamContainer from "../Components/DreamContainer"
+import { TestData } from "../constants/test"
 
 function Home() {
     return (
         <div className='bg-background'>
             <Navbar/>
+            <div className=" px-10 py-10 mt-28 grid grid-cols-3 gap-5 ">
+                {TestData.map((t)=>(
+                    <DreamContainer
+                        dreamName={t.dreamName}
+                        dreamDate={t.dreamDate}
+                        dreamDescription={t.dreamDescription}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
