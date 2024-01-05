d4e975e24507efdaf3abcb23b57fcc3578160ff4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-05 17:00:58 +0800

    update

--- src/Modals/LogOut.jsx
+++ src/Modals/LogOut.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -11,13 +12,20 @@ function LogOut({onClose}) {
         }
     }
 
+    const handleDelete = () =>{
+        axios.get('http://localhost:8001/logout')
+        .then(res => {
+            location.reload(true)
+        }).catch(err => console.log(err))
+    }
+
     return (
         <div ref={modalRef} onClick={closeModal} className='fixed inset-0 flex justify-center items-center text-text bg-background bg-opacity-70 backdrop-blur-sm z-50'>
             <div className='px-16 py-8 flex flex-col justify-center items-center gap-4 bg-container2 rounded-md'>
                 <h1 className='text-2xl'>Are you sure to Log out DreamSnare?</h1>
                 <div className='flex gap-6'>
-                    <button onClick={onClose} className = 'py-3 px-8 bg-error hover:bg-errordark ease-in-out duration-300 rounded-md'>No</button>
-                    <Link to='/'><button className = 'py-3 px-8 bg-yes hover:bg-yesdark ease-in-out duration-300 rounded-md'>Yes</button></Link>
+                    <button className = 'py-3 px-8 bg-error hover:bg-errordark ease-in-out duration-300 rounded-md'>No</button>
+                    <Link to='/'><button onClick={handleDelete} className = 'py-3 px-8 bg-yes hover:bg-yesdark ease-in-out duration-300 rounded-md'>Yes</button></Link>
                 </div>
             </div>  
         </div>
