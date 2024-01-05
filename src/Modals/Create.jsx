9ed13dddfc88c5e3f0a1267d437d5e79f5b76da7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-05 14:08:37 +0800

    update

--- src/Modals/Create.jsx
+++ src/Modals/Create.jsx
@@ -12,7 +12,7 @@ function Create({onClose}) {
     }
 
     return (
-        <div ref={modalRef} onClick={closeModal} className='fixed inset-0 flex justify-center items-center text-text bg-background bg-opacity-25 backdrop-blur-sm '>
+        <div ref={modalRef} onClick={closeModal} className='fixed inset-0 flex justify-center items-center text-text bg-background bg-opacity-70 backdrop-blur-sm '>
             <form className="px-16 py-8 flex flex-col bg-container2 rounded-md text-left transition-all ">
                 <div className='flex items-center gap-3'>
                     <label htmlFor="">Name your Dream:</label>
@@ -27,9 +27,9 @@ function Create({onClose}) {
                         className='py-1 px-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'/>
                 </div>
                 <label className='my-6'>Describe your dream!</label>
-                <textarea name='dreamDescription' rows={15} cols={30} className='py-1 px-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'></textarea>
+                <textarea name='dreamDescription' rows={10} cols={20} className='p-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'></textarea>
                     <div className='mt-6 justify-end flex gap-4'>
-                        <button onClick={onClose} className='py-3 px-5 rounded-md text-background bg-primary text-white hover:bg-accent ease-in-out duration-300'>Cancel</button>
+                        <button onClick={onClose} className='py-3 px-5 rounded-md text-text bg-error text-white hover:bg-errordark ease-in-out duration-300'>Cancel</button>
                         <button type='submit' className='py-3 px-5 rounded-md text-background bg-primary text-white hover:bg-accent ease-in-out duration-300'>Submit</button>
                     </div>
             </form> 
--- src/constants/test.js
+++ src/constants/test.js
@@ -1,25 +1,36 @@
 export const TestData = [
     {
+        userName:"Loooook12",
         dreamName:"Lobot",
         dreamDate:"January 1, 2024",
         dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
     },
     {
+        userName:"Loooook13",
         dreamName:"Lobot",
         dreamDate:"January 1, 2024",
         dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
     },
     {
+        userName:"Loooook14",
         dreamName:"Lobot",
         dreamDate:"January 1, 2024",
         dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
     },
     {
+        userName:"Loooook15",
         dreamName:"Lobot",
         dreamDate:"January 1, 2024",
         dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
     },
     {
+        userName:"Loooook16",
+        dreamName:"Lobot",
+        dreamDate:"January 1, 2024",
+        dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
+    },
+    {
+        userName:"Loooook17",
         dreamName:"Lobot",
         dreamDate:"January 1, 2024",
         dreamDescription:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt architecto. Sed iste voluptatum aliquid consequatur beatae aut id nam, sequi voluptatibus quasi, debitis quae eaque et, pariatur necessitatibus dicta."
