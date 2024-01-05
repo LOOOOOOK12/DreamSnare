6e9b6dbd96bcd28ddd2a34a1fb11e44f10a369bc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-05 14:08:34 +0800

    update

--- src/Components/DreamContainer.jsx
+++ src/Components/DreamContainer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+import Edit from '../Modals/Edit'
+import Delete from '../Modals/Delete'
 
-function DreamContainer({dreamName, dreamDate, dreamDescription}) {
+function DreamContainer({userName,dreamName, dreamDate, dreamDescription}) {
+
+    const [showEdit, setShowEdit] = useState(false)
+    const [showDelete, setShowDelete] = useState(false)
+
     return (
         <div className='py-12 px-8 bg-container2 flex flex-col text-text rounded-lg'>
+            <h1 className='mb-4 text-3xl'>{userName}</h1>
             <div className='mb-4 flex gap-4'>
                 <h1>Dream Name:</h1>
                 <h1>{dreamName}</h1>
@@ -15,9 +23,11 @@ function DreamContainer({dreamName, dreamDate, dreamDescription}) {
                 {dreamDescription}
             </p>
             <div className='mt-5 flex justify-end gap-4'>
-                <button className='py-3 px-7 rounded-md bg-error text-white hover:bg-errordark ease-in-out duration-300'>Delete</button>
-                <button className='py-3 px-7 rounded-md text-background bg-primary hover:bg-accent ease-in-out duration-300'>Edit</button>
+                <button onClick={() => setShowDelete(true)} className='py-3 px-7 rounded-md bg-error font-semibold text-white hover:bg-errordark ease-in-out duration-300'>Delete</button>
+                <button onClick={() => setShowEdit(true)} className='py-3 px-7 rounded-md text-background font-semibold  bg-primary hover:bg-accent ease-in-out duration-300'>Edit</button>
             </div>
+            {showEdit && <Edit onClose = {() => setShowEdit(false)}/>}
+            {showDelete && <Delete onClose = {() => setShowDelete(false)}/>}
         </div>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Create from '../Modals/Create'
+import LogOut from '../Modals/LogOut'
 
 function Navbar() {
 
     const [showModal, setShowModal] = useState(false)
+    const [logOut, setShowLogOut] = useState(false)
 
     return (
         <div className=' w-full'>
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center bg-container2 text-text'>
-                <h1 className='text-2xl font-semibold'>DreamSnare.net</h1>
+                <h1 className='text-2xl font-semibold cursor-default'>DreamSnare.net</h1>
                 <div className='flex gap-4'>
-                    <button className='py-3 px-7 rounded-md text-background bg-primary text-white hover:bg-accent ease-in-out duration-300'
-                    onClick={()=>setShowModal(true)}
+                    <button className='py-3 px-7 rounded-md text-background bg-primary font-semibold text-white hover:bg-accent ease-in-out duration-300'
+                        onClick={()=>setShowModal(true)}
                     >Create</button>
-                    <Link to='/'><button className='py-3 px-7 rounded-md text-background bg-primary text-white hover:bg-accent ease-in-out duration-300'>Log-Out</button></Link>
+                    <button className='py-3 px-7 rounded-md bg-error font-semibold text-white hover:bg-errordark ease-in-out duration-300'
+                        onClick={()=>setShowLogOut(true)}
+                    >Log-Out</button>
                 </div>
                 {showModal && <Create onClose={() => setShowModal(false)}/>}
+                {logOut && <LogOut onClose={() => setShowLogOut(false)}/>}
             </nav>
         </div>
         )
