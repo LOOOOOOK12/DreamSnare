a723e55df3e617e346a7bad7d6cf798edf767a3e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-05 14:08:48 +0800

    update

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -41,21 +41,21 @@ function SignUp() {
             <div className='px-10 py-14 flex flex-col gap-6 bg-container2 text-text rounded-md'>
                 <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
                     <div className='flex flex-col'>
-                        <label className='mb-2'>Enter Username:</label>
+                        <label htmlFor='Username' className='mb-2'>Enter Username:</label>
                         <input type="text" name="username" placeholder='exampleUsername'
                             className='py-1 px-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'
                             onChange={handleChange}    
                         />
                     </div>
                     <div className='flex flex-col'>
-                        <label className='mb-2'>Enter Email:</label>
+                        <label htmlFor='Email' className='mb-2'>Enter Email:</label>
                         <input type="text" name="email" placeholder='example@email.com'
                             className='py-1 px-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'
                             onChange={handleChange}
                         />
                     </div>
                     <div className='flex flex-col'>
-                        <label className='mb-2'>Enter Password:</label>
+                        <label htmlFor='Password' className='mb-2'>Enter Password:</label>
                         <input type="password" name="password" 
                             className='py-1 px-1 rounded-md bg-container2 border-2 border-accent focus:outline-none focus:border-secondary'
                             onChange={handleChange}
