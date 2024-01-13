007b818f4a7763a6e1f916b07ccf4558b50fbbba
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-13 19:02:48 +0800

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,10 +13,10 @@ function Navbar({user_ID}) {
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center bg-container2 text-text'>
                 <h1 className='text-2xl font-semibold cursor-default'>DreamSnare.net</h1>
                 <div className='flex gap-4'>
-                    <button className='py-3 px-7 rounded-md text-background bg-primary font-semibold text-white hover:bg-accent ease-in-out duration-300'
+                    <button className='py-3 px-7 max-[860px]:hidden rounded-md text-background bg-primary font-semibold text-white hover:bg-accent ease-in-out duration-300'
                         onClick={()=>setShowModal(true)}
                     >Create</button>
-                    <button className='py-3 px-7 rounded-md bg-error font-semibold text-white hover:bg-errordark ease-in-out duration-300'
+                    <button className='py-3 px-7 max-[860px]:hidden rounded-md bg-error font-semibold text-white hover:bg-errordark ease-in-out duration-300'
                         onClick={()=>setShowLogOut(true)}
                     >Log-Out</button>
                 </div>
