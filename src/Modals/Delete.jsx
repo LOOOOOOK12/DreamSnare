1db174c4941b64f19af2fa5775cd1185868699fc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-20 17:06:19 +0800

    update

--- src/Modals/Delete.jsx
+++ src/Modals/Delete.jsx
@@ -12,7 +12,7 @@ function Delete({onClose, onYes}) {
 
     return (
         <div ref={modalRef} onClick={closeModal} className='fixed inset-0 flex justify-center items-center text-text bg-background bg-opacity-70 backdrop-blur-sm z-50'>
-            <div className='px-16 py-8 flex flex-col justify-center items-center gap-4 bg-container2 rounded-md'>
+            <div className='px-10 py-8 flex flex-col justify-center text-center items-center gap-4 bg-container2 rounded-md'>
                 <h1 className='text-2xl'>Are you sure to delete your Dream?</h1>
                 <div className='flex gap-6'>
                     <button onClick={onClose} className = 'py-3 px-8 bg-error hover:bg-errordark rounded-md'>No</button>
