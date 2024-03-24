74c51b8e3329eec5fa6287d9a2208fb16a91cbd8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-24 18:43:23 +0800

    update

--- Likod/server.js
+++ Likod/server.js
@@ -184,6 +184,6 @@ app.delete('/deleteDream/:dream_ID', (req, res) => {
     });
 });
 
-// app.listen(8001, ()=> {
-//     console.log("Server is Open")
-// })
+app.listen(8001, ()=> {
+    console.log("Server is Open")
+})
