8a48aa60bb8ab8aea4e64e8b6c0f698557135c60
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-24 18:21:19 +0800

    update

--- Harap/src/Pages/SignUp.jsx
+++ Harap/src/Pages/SignUp.jsx
@@ -43,7 +43,7 @@ function SignUp() {
     
         setValidationErrors({});
     
-        axios.post('http://localhost:8001/signup', values)
+        axios.post('https://dream-snare-api.vercel.app/signup', values)
             .then(res => {
                 console.log('Registered Successfully!');
                 navigate('/');
